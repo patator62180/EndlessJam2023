50fcfae53cf0df89a0399dda75060b49f4860466
David Corticchiato <david.corticchiato@gmail.com>
2023-11-27 19:25:56 -0500

    chunks pool

--- scenes/map_generation/map_generation.gd
+++ scenes/map_generation/map_generation.gd
@@ -9,6 +9,7 @@ const FROG_GROUP = 'frog'
 @export var steep_chunks_bank: Array[PackedScene]
 @export var steep_obstacle_chunks_bank: Array[PackedScene]
 @export var chunks_root: Node2D
+@export var chunks_pool_root: Node2D
 @export var collision_polygon: CollisionPolygon2D
 @export var enable_collision_polygon: bool = true
 
@@ -22,17 +23,21 @@ const FROG_GROUP = 'frog'
 @export var average_chunk_width: int = 652
 @export var min_chunks_before_obstacle: int = 6
 @export var max_chunks_before_obstacle: int = 10
+@export var chunks_pools_size: int = 20
 
 enum ChunkType {Square, Steep}
 
+
 class ChunkDef:
-    var packed_chunk: PackedScene
+    var pool: Array[Chunk]
     var type: ChunkType
     var obstacle: bool
+
 
 class Bounds:
     var min: int
     var max: int
+
 
 var last_obstacle_position: int = 0
 var chunk_count_since_type_change: int = 0
@@ -43,6 +48,43 @@ var rng: RandomNumberGenerator
 var last_chunk_index: int = -1
 var points: Array[Vector2] = []
 var frog: Node2D
+var square_chunks_pool: Array[Array] = []
+var steep_chunks_pool: Array[Array] = []
+var steep_obstacle_chunks_pool: Array[Array] = []
+
+
+func build_chunk_pools():
+    build_chunk_pool_type(ChunkType.Square, false)
+    build_chunk_pool_type(ChunkType.Steep, false)
+    build_chunk_pool_type(ChunkType.Steep, true)
+
+
+func build_chunk_pool_type(type: ChunkType, obstacle: bool):
+    var bank: Array[PackedScene] = []
+    var pool: Array[Array] = []
+    
+    if type == ChunkType.Square:
+        bank = square_chunks_bank
+        pool = square_chunks_pool
+    elif type == ChunkType.Steep and obstacle == true:
+        bank = steep_obstacle_chunks_bank
+        pool = steep_obstacle_chunks_pool
+    else:
+        bank = steep_chunks_bank
+        pool = steep_chunks_pool
+   
+    for packed_chunk in bank:
+        var typed_pool: Array[Chunk] = []
+
+        for index in range(chunks_pools_size):
+            var chunk = packed_chunk.instantiate() as Chunk
+    
+            chunks_pool_root.add_child(chunk)
+            chunk.position = Vector2.ZERO
+            chunk.disable_segment()
+            typed_pool.append(chunk)
+
+        pool.append(typed_pool)
 
 
 func generate_next_threshold(type: ChunkType):
@@ -72,13 +114,13 @@ func toggle_type(type):
     return ChunkType.Square if type == ChunkType.Steep else ChunkType.Steep
 
 
-func pick_bank(type: ChunkType, obstacle: bool):
+func pick_pool(type: ChunkType, obstacle: bool):
     if type == ChunkType.Square:
-        return square_chunks_bank
+        return square_chunks_pool
     elif type == ChunkType.Steep and obstacle == true:
-        return steep_obstacle_chunks_bank
+        return steep_obstacle_chunks_pool
     else:
-        return steep_chunks_bank
+        return steep_chunks_pool
 
 
 func append_chunk_def():
@@ -100,26 +142,27 @@ func append_chunk_def():
         chunk_def.type = type
 
     chunk_count_since_type_change += 1
-    var bank = pick_bank(chunk_def.type, chunk_def.obstacle)
+    var pool = pick_pool(chunk_def.type, chunk_def.obstacle)
 
-    chunk_def.packed_chunk = bank[rng.randi_range(0, len(bank) - 1)]
+    chunk_def.pool = pool[rng.randi_range(0, len(pool) - 1)]
     chunk_defs.append(chunk_def)
 
 
 func spawn_chunk(index: int):
-    var chunk = chunk_defs[index].packed_chunk.instantiate() as Chunk
+    var chunk: Chunk = chunk_defs[index].pool.pop_front()
     var previous_chunk = chunks[last_chunk_index] if len(chunks) > 0 else start_chunk
 
     if len(points) == 0:
         points.append(start_chunk.position + start_chunk.get_segment().a)
 
+    chunks_pool_root.remove_child(chunk)
+    chunks_root.add_child(chunk)
+
     chunk.position = Vector2(previous_chunk.get_segment().b.x + previous_chunk.position.x - chunk.get_segment().a.x, previous_chunk.position.y + previous_chunk.get_segment().b.y - chunk.get_segment().a.y)
 
     var new_point = chunk.position + chunk.get_segment().a
 
-    chunks_root.add_child(chunk);
     chunks.append(chunk)
-    chunk.disable_segment()
     remove_extra_points(new_point)
 
     if index > len(points) - 1:
@@ -147,10 +190,8 @@ func populate_chunks(target: int):
             var absolute_index = initial_last_chunk_index + index + 1
             
             if absolute_index > len(chunk_defs) - 1:
-#                    print("popuplate chunk_def %d" % index)
                 append_chunk_def()
             if absolute_index > last_chunk_index:
-#                    print("spawn chunk %d" % index)
                 spawn_chunk(absolute_index)
                 draw_collision_polygon()
 
@@ -184,6 +225,7 @@ func _ready():
         printerr('No frog found in tree, cannot update map automatically')
 
     rng = RandomNumberGenerator.new()
+    build_chunk_pools()
     populate_chunks(initial_chunks_count)
 
     
