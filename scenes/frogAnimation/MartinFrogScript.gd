1e8b12bc668d4062af696f750f88d589154941d7
Martin Briand <martinbriands@gmail.com>
2023-11-27 23:52:50 -0500

    best walking yet
    so proud guys

--- scenes/frogAnimation/MartinFrogScript.gd
+++ scenes/frogAnimation/MartinFrogScript.gd
@@ -3,11 +3,11 @@ extends CharacterBody2D
 const SPEED = 700
 const JUMP_VELOCITY = -900.0
 
-const FOOT_DISTANCE = 150
-const FOOT_DETECTION = 100
+const FOOT_DISTANCE = 800
+const FOOT_DETECTION = 150
 const FOOT_HEIGHT = 30
 
-const FOOT_TIMER_INCREMENT = 0.2
+const FOOT_TIMER_INCREMENT = 0.1
 
 const BALL_GROUP = 'ball'
 
@@ -51,12 +51,21 @@ var holding
 
 var rockThrowTarget
 
+var legL
+var legR
+var footL
+var footR
+
 func _ready():
     legLTarget = $"FootL"
+    legL = $Skeleton2D/Hip/LegL
+    footL = $Skeleton2D/Hip/LegL/ForeLegL/ForeForeLegL/FootL
     raycastLegL = $"Skeleton2D/RaycastLegL"
     raycastLegL2 = $"Skeleton2D/RaycastLegL2"    
     
     legRTarget = $"FootR"
+    legR = $Skeleton2D/Hip/LegR
+    footR = $Skeleton2D/Hip/LegR/ForeLegR/ForeForeLegR/FootR
     raycastLegR = $"Skeleton2D/RaycastLegR"
     raycastLegR2 = $"Skeleton2D/RaycastLegR2"
     
@@ -81,6 +90,7 @@ func _ready():
     
     safeRockArea.body_entered.connect(touch_ball)
     safeRockArea.body_exited.connect(untouch_ball)
+    
 
 func touch_ball(body: Node2D):
     if body.is_in_group(BALL_GROUP):
@@ -153,15 +163,15 @@ func _physics_process(delta):
     #if touchingBall and not ball == null:
     #	ball.linear_velocity = velocity
     
-var timerL = 1
-var oldTargetL
-var currentTargetL
-var midTargetL
+@export var timerL = 1
+@export var oldTargetL = Vector2()
+@export var currentTargetL = Vector2()
+@export var midTargetL = Vector2()
 
-var timerR = 1
-var oldTargetR
-var currentTargetR
-var midTargetR
+@export var timerR = 1
+@export var oldTargetR = Vector2()
+@export var currentTargetR = Vector2()
+@export var midTargetR = Vector2()
 
 var wasMoving
     
@@ -188,67 +198,104 @@ func raycastLegs():
     var footSpeedModifier = 1 if ! touchingBall else 0.5
     
     var floorAngle = 0 if !is_on_floor() else get_floor_angle()
+    
+    
+    #moveLeg(raycastLegL, raycastLegL2, footL, legL, timerL, currentTargetL, oldTargetL, midTargetL, legLTarget, modifier, floorAngle, moving, justStopped, footSpeedModifier)
+    #moveLeg(raycastLegR, raycastLegR2, footR, legR, timerR, currentTargetR, oldTargetR, midTargetR, legRTarget, modifier, floorAngle, moving, justStopped, footSpeedModifier)
 
-    #raycastLegL2.position = raycastLegL.position + Vector2(modifier, 0) * FOOT_DETECTION / scaleScalar
-    #raycastLegL2.rotation = deg_to_rad(modifier * -footAngleModifier)
+    var distanceL = legL.global_position.distance_to(footL.global_position)
+    var distanceR = legR.global_position.distance_to(footR.global_position)
+
     raycastLegL2.position = raycastLegL.position + Vector2(modifier, 0).normalized().rotated(-floorAngle) * FOOT_DETECTION / scaleScalar
     var rayCastL = raycastLegL if !moving || !is_on_floor() else raycastLegL2
-    
+
     var colliderL = rayCastL.get_collider()
     if colliderL != null:
         var pos = rayCastL.get_collision_point()
-        
-        if currentTargetL == null || (currentTargetL.distance_to(pos) >= FOOT_DISTANCE && timerR == 1) || justStopped:
-            #var normal = rayCastL.get_collision_normal()
-            if currentTargetL == null:
-                oldTargetL = pos
-                currentTargetL = pos
-                midTargetL = pos
-                timerL = 1
-            else:
-                oldTargetL = currentTargetL if !justStopped else legLTarget.get_global_position()
-                currentTargetL = pos #+ normal
-                var midPoint = (oldTargetL + currentTargetL) / 2
-                var midPointModifier = rayCastL.get_collision_normal() * FOOT_HEIGHT / scaleScalar if !justStopped else Vector2.ZERO
-                midTargetL = midPoint + midPointModifier
-                timerL = 0
-            
+
+        if distanceL >= FOOT_DISTANCE * scaleScalar and timerL == 1 and timerR == 1 and distanceL > distanceR:
+            oldTargetL = footL.global_position
+            currentTargetL = pos
+            var midPoint = (oldTargetL + currentTargetL) / 2
+            var midPointModifier = rayCastL.get_collision_normal() * FOOT_HEIGHT / scaleScalar if !justStopped else Vector2.ZERO
+            midTargetL = midPoint + midPointModifier
+            timerL = 0
+
+        if currentTargetL == null:
+            oldTargetL = pos
+            currentTargetL = pos
+            midTargetL = pos
+            timerL = 1
+
         var target = _quadratic_bezier(oldTargetL, midTargetL, currentTargetL, timerL)
-        timerL = min(timerL + FOOT_TIMER_INCREMENT * footSpeedModifier, 1)
-            
+        timerL = min(timerL + FOOT_TIMER_INCREMENT, 1)
+
         legLTarget.set_global_position(target)
     else:
         currentTargetL = null
-
-    #raycastLegR2.position = raycastLegR.position + Vector2(modifier, 0) * FOOT_DETECTION / scaleScalar
-    #raycastLegR2.rotation = deg_to_rad(modifier * -footAngleModifier)
-    raycastLegR2.position = raycastLegR.position + Vector2(modifier, 0).normalized().rotated(-floorAngle) * FOOT_DETECTION / scaleScalar 
+        
+    raycastLegR2.position = raycastLegR.position + Vector2(modifier, 0).normalized().rotated(-floorAngle) * FOOT_DETECTION / scaleScalar
     var rayCastR = raycastLegR if !moving || !is_on_floor() else raycastLegR2
-    
+
     var colliderR = rayCastR.get_collider()
     if colliderR != null:
         var pos = rayCastR.get_collision_point()
-        
-        if currentTargetR == null || (currentTargetR.distance_to(pos) >= FOOT_DISTANCE && timerL == 1) || justStopped:
-            if currentTargetR == null:
-                oldTargetR = pos
-                currentTargetR = pos
-                midTargetR = pos
-                timerR = 1
-            else:
-                oldTargetR = currentTargetR if !justStopped else legRTarget.get_global_position()
-                currentTargetR = pos #+ normal
-                var midPoint = (oldTargetR + currentTargetR) / 2
-                var midPointModifier = rayCastR.get_collision_normal() * FOOT_HEIGHT / scaleScalar if !justStopped else Vector2.ZERO
-                midTargetR = midPoint + midPointModifier
-                timerR = 0
-            
+
+        if distanceR >= FOOT_DISTANCE * scaleScalar and timerR == 1 and timerL == 1 and distanceR > distanceL:
+            oldTargetR = footR.global_position
+            currentTargetR = pos
+            var midPoint = (oldTargetR + currentTargetR) / 2
+            var midPointModifier = rayCastR.get_collision_normal() * FOOT_HEIGHT / scaleScalar if !justStopped else Vector2.ZERO
+            midTargetR = midPoint + midPointModifier
+            timerR = 0
+
+        if currentTargetR == null:
+            oldTargetR = pos
+            currentTargetR = pos
+            midTargetR = pos
+            timerR = 1
+
         var target = _quadratic_bezier(oldTargetR, midTargetR, currentTargetR, timerR)
-        timerR = min(timerR + FOOT_TIMER_INCREMENT * footSpeedModifier, 1)
-            
+        timerR = min(timerR + FOOT_TIMER_INCREMENT, 1)
+
         legRTarget.set_global_position(target)
     else:
         currentTargetR = null
+
+
+
+
+
+
+
+#    raycastLegR2.position = raycastLegR.position + Vector2(modifier, 0).normalized().rotated(-floorAngle) * FOOT_DETECTION / scaleScalar 
+#    var rayCastR = raycastLegR if !moving || !is_on_floor() else raycastLegR2
+#
+#    var colliderR = rayCastR.get_collider()
+#    if colliderR != null:
+#        var pos = rayCastR.get_collision_point()
+#
+#        if currentTargetR == null || (legR.global_position.distance_to(footR.global_position) >= FOOT_DISTANCE * scaleScalar && timerL == 1 ) || justStopped:
+#            if currentTargetR == null:
+#                oldTargetR = pos
+#                currentTargetR = pos
+#                midTargetR = pos
+#                timerR = 1
+#            else:
+#                #oldTargetR = currentTargetR if !justStopped else legRTarget.get_global_position()
+#                oldTargetR = footR.global_position
+#                currentTargetR = pos #+ normal
+#                var midPoint = (oldTargetR + currentTargetR) / 2
+#                var midPointModifier = rayCastR.get_collision_normal() * FOOT_HEIGHT / scaleScalar if !justStopped else Vector2.ZERO
+#                midTargetR = midPoint + midPointModifier
+#                timerR = 0
+#
+#        var target = _quadratic_bezier(oldTargetR, midTargetR, currentTargetR, timerR)
+#        timerR = min(timerR + FOOT_TIMER_INCREMENT * footSpeedModifier, 1)
+#
+#        legRTarget.set_global_position(target)
+#    else:
+#        currentTargetR = null
         
     wasMoving = moving
 
