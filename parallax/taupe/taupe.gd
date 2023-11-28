ef67d51411c0d82c53b836e99e4f2f2dd5b525b7
Martin Briand <martinbriands@gmail.com>
2023-11-28 09:52:53 -0500

    better champignon, taupe and skull collision

--- parallax/taupe/taupe.gd
+++ parallax/taupe/taupe.gd
@@ -1,6 +1,25 @@
 extends AnimatedSprite2D
 
+@export var bounce_collision = Area2D
+
+var ball 
 
 func _ready():
     var rnd = RandomNumberGenerator.new()
     $AnimationPlayer.advance(rnd.randf_range(0.0,5.0))
+    
+    bounce_collision.body_entered.connect(touch_ball)
+    bounce_collision.body_exited.connect(untouch_ball)
+    
+func _process(delta):
+    if ball != null:
+        ball.apply_central_impulse(Vector2.UP.rotated(rotation) * 10000)
+        ball = null
+    
+func touch_ball(body):
+    if body.is_in_group("ball"):
+        ball = body
+        
+func untouch_ball(body):
+    if body.is_in_group("ball"):
+        ball = null
