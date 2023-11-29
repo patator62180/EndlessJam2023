02f2acbef2800575ecb577ad09793d4559e9baf5
patator62180 <samuel.terrasviola@gmail.com>
2023-11-28 20:43:04 -0500

    randomize bouncing

--- parallax/champignon/champignon.gd
+++ parallax/champignon/champignon.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
+@export var bounceMin = 8000
+@export var bounceMax = 20000
+
 
 func _ready():    
     var rnd = RandomNumberGenerator.new()
+    var rndf = rnd.randf()
     $AnimatedSprite2D2.material = $AnimatedSprite2D2.material.duplicate()
-    $AnimatedSprite2D2.material.set_shader_parameter("hueShift", rnd.randf())
+    $AnimatedSprite2D2.material.set_shader_parameter("hueShift", rndf)
     var rndFrame = rnd.randi_range(0,4); 
     $AnimatedSprite2D.frame = rndFrame
     $AnimatedSprite2D2.frame = rndFrame
+    $BounceCollision.bounce_power = (bounceMax-bounceMin)*rndf + bounceMin
--- parallax/taupe/bounce.gd
+++ parallax/taupe/bounce.gd
@@ -1,8 +1,9 @@
 extends Area2D
 
-var ball 
-
 @export var directional_bounce = false
+
+var ball 
+var bounce_power = 10000
 
 func _ready(): 
     body_entered.connect(touch_ball)
@@ -11,11 +12,11 @@ func _ready():
 func _process(delta):
     if ball != null:
         if !directional_bounce:
-            ball.apply_central_impulse(Vector2.UP.rotated(global_rotation) * 10000)
+            ball.apply_central_impulse(Vector2.UP.rotated(global_rotation) * bounce_power)
         else:
             ball.freeze = true
             ball.freeze = false
-            var direction = (ball.global_position - global_position).normalized() * 10000
+            var direction = (ball.global_position - global_position).normalized() * bounce_power
             ball.apply_central_impulse(direction)
         ball = null
     
